84984becfe88b0f0804f79517ebe82df29eba35a
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-10 22:40:24 +0300

    added function for romove and add classes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
 
+    $('.info-tabs__item').on('click', function(e){
+        e.preventDefault();
+        $('.info-tabs__item').removeClass('info-tabs__item--active');
+        $(this).addClass('info-tabs__item--active');
+    });
+
 });
