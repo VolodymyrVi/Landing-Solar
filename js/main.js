c4acc4ec44b41ef928b643b7d8902d1c6d7eae1b
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-10 22:50:26 +0300

    added function remove and add classes for info-tabs__content

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ $(function(){
         e.preventDefault();
         $('.info-tabs__item').removeClass('info-tabs__item--active');
         $(this).addClass('info-tabs__item--active');
+
+        $('.info-tabs__content').removeClass('info-tabs__content--active');
+        $($(this).attr('href')).addClass('info-tabs__content--active')
     });
 
 });
